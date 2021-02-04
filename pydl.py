ec39e29e9974f907fd0b941c85cfa60143a81bad
Jake <jake@tkb.mss.co.jp>
2021-02-04 13:51:57 +0900

    Add NO_PROXY for localhost.

--- pydl.py
+++ pydl.py
@@ -1,7 +1,8 @@
-
+import os
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+os.environ['NO_PROXY'] = '127.0.0.1'
+page = requests.get("http://127.0.0.1:3000/pages/main.html")
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify())
