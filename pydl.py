2059e4607b8cc40592865a3172e83cc6a92a5ce0
Jake <jake@tkb.mss.co.jp>
2021-02-08 09:28:55 +0900

    Set pydl back to original

--- pydl.py
+++ pydl.py
@@ -1,12 +1,9 @@
 import os
 import sys
 import logging
-import requests
-from bs4 import BeautifulSoup
 
 from settings import Settings
 from log import LogSettings
-from pydlr import PyDlr
 
 # Logger
 logger = logging.getLogger(__name__)
@@ -34,35 +31,10 @@ def main():
 
 
 if __name__ == '__main__':
-    # try:
-    #     main()
-    # except Exception as e:
-    #     logger.error(f'An unexpected error occurred. {str(e)}')
-    #     logger.exception(e)
-    # finally:
-    #     LogSettings.close()
-
-    LogSettings.init()
-    settings = Settings(sys.argv[1:])
-
     try:
-        items = []
-        for idx in range(1, 21):
-            item = {'url': f'http://www.xkcd.com/{idx}'}
-            items.append(item)
-
-        def callback(result):
-            # if result.isSuccess:
-            #     print(f'Downloaded {len(result.getText())} bytes from {result.url}')
-            # else:
-            #     print(f'Failed to download {result.url}, {result.failureReason}')
-            print(f'Executing {"success" if result.isSuccess else "failure"} callback for {result.url}.')
-
-        pydlr = PyDlr(items, 3, callback)
-
-        pydlr.start()
-        pydlr.join()
-
-    except (SystemExit, KeyboardInterrupt):
-        pydlr.kill()
-        logger.info('Program stopped.')
+        main()
+    except Exception as e:
+        logger.error(f'An unexpected error occurred. {str(e)}')
+        logger.exception(e)
+    finally:
+        LogSettings.close()
